d91398a005b17926cabf5c2be9905cafbdd499c1
Anay Wadhera <anay1018@gmail.com>
2018-08-10 08:52:41 +0000

    add a lot of missing libs, related to keystore and camera because the log was complaining about them

--- angler/angler-vendor-blobs.mk
+++ angler/angler-vendor-blobs.mk
@@ -33,7 +33,87 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/angler/proprietary/lib/libimsmedia_jni.so:system/lib/libimsmedia_jni.so \
     vendor/huawei/angler/proprietary/lib/libmm-qcamera.so:system/lib/libmm-qcamera.so \
     vendor/huawei/angler/proprietary/lib/gatekeeper.msm8994.so:vendor/lib/hw/gatekeeper.msm8994.so \
+    vendor/huawei/angler/proprietary/lib/keystore.msm8994.so:vendor/lib/hw/keystore.msm8994.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_c2d_module.so:vendor/lib/libmmcamera2_c2d_module.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_cpp_module.so:vendor/lib/libmmcamera2_cpp_module.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_frame_algorithm.so:vendor/lib/libmmcamera2_frame_algorithm.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_iface_modules.so:vendor/lib/libmmcamera2_iface_modules.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_imglib_modules.so:vendor/lib/libmmcamera2_imglib_modules.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_is.so:vendor/lib/libmmcamera2_is.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_isp_modules.so:vendor/lib/libmmcamera2_isp_modules.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_pproc_modules.so:vendor/lib/libmmcamera2_pproc_modules.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_pp_buf_mgr.so:vendor/lib/libmmcamera2_pp_buf_mgr.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_q3a_core.so:vendor/lib/libmmcamera2_q3a_core.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_sensor_debug.so:vendor/lib/libmmcamera2_sensor_debug.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_sensor_modules.so:vendor/lib/libmmcamera2_sensor_modules.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_stats_algorithm.so:vendor/lib/libmmcamera2_stats_algorithm.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_stats_modules.so:vendor/lib/libmmcamera2_stats_modules.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_vpe_module.so:vendor/lib/libmmcamera2_vpe_module.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera2_wnr_module.so:vendor/lib/libmmcamera2_wnr_module.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_cac2_lib.so:vendor/lib/libmmcamera_cac2_lib.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_dw9761b_eeprom.so:vendor/lib/libmmcamera_dw9761b_eeprom.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_eeprom_util.so:vendor/lib/libmmcamera_eeprom_util.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_eztune_module.so:vendor/lib/libmmcamera_eztune_module.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_faceproc.so:vendor/lib/libmmcamera_faceproc.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_imglib.so:vendor/lib/libmmcamera_imglib.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_imx179_liteon.so:vendor/lib/libmmcamera_imx179_liteon.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_imx179_sunny.so:vendor/lib/libmmcamera_imx179_sunny.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_imx377.so:vendor/lib/libmmcamera_imx377.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_abcc44.so:vendor/lib/libmmcamera_isp_abcc44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_abf44.so:vendor/lib/libmmcamera_isp_abf44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_bcc44.so:vendor/lib/libmmcamera_isp_bcc44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_be_stats44.so:vendor/lib/libmmcamera_isp_be_stats44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_bf_scale_stats46.so:vendor/lib/libmmcamera_isp_bf_scale_stats46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_bf_stats44.so:vendor/lib/libmmcamera_isp_bf_stats44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_bg_stats46.so:vendor/lib/libmmcamera_isp_bg_stats46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_bhist_stats44.so:vendor/lib/libmmcamera_isp_bhist_stats44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_bpc44.so:vendor/lib/libmmcamera_isp_bpc44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_chroma_enhan40.so:vendor/lib/libmmcamera_isp_chroma_enhan40.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_chroma_suppress40.so:vendor/lib/libmmcamera_isp_chroma_suppress40.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_clamp_encoder40.so:vendor/lib/libmmcamera_isp_clamp_encoder40.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_clamp_video40.so:vendor/lib/libmmcamera_isp_clamp_video40.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_clamp_viewfinder40.so:vendor/lib/libmmcamera_isp_clamp_viewfinder40.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_clf46.so:vendor/lib/libmmcamera_isp_clf46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_color_correct46.so:vendor/lib/libmmcamera_isp_color_correct46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_color_xform_encoder46.so:vendor/lib/libmmcamera_isp_color_xform_encoder46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_color_xform_video46.so:vendor/lib/libmmcamera_isp_color_xform_video46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_color_xform_viewfinder46.so:vendor/lib/libmmcamera_isp_color_xform_viewfinder46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_cs_stats46.so:vendor/lib/libmmcamera_isp_cs_stats46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_demosaic44.so:vendor/lib/libmmcamera_isp_demosaic44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_demux40.so:vendor/lib/libmmcamera_isp_demux40.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_fovcrop_encoder46.so:vendor/lib/libmmcamera_isp_fovcrop_encoder46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_fovcrop_video46.so:vendor/lib/libmmcamera_isp_fovcrop_video46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_fovcrop_viewfinder46.so:vendor/lib/libmmcamera_isp_fovcrop_viewfinder46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_gamma44.so:vendor/lib/libmmcamera_isp_gamma44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_gic46.so:vendor/lib/libmmcamera_isp_gic46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_gtm46.so:vendor/lib/libmmcamera_isp_gtm46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_hdr46.so:vendor/lib/libmmcamera_isp_hdr46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_hdr_be_stats46.so:vendor/lib/libmmcamera_isp_hdr_be_stats46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_ihist_stats46.so:vendor/lib/libmmcamera_isp_ihist_stats46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_linearization40.so:vendor/lib/libmmcamera_isp_linearization40.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_ltm44.so:vendor/lib/libmmcamera_isp_ltm44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_mce40.so:vendor/lib/libmmcamera_isp_mce40.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_mesh_rolloff44.so:vendor/lib/libmmcamera_isp_mesh_rolloff44.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_pedestal_correct46.so:vendor/lib/libmmcamera_isp_pedestal_correct46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_rs_stats46.so:vendor/lib/libmmcamera_isp_rs_stats46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_scaler_encoder46.so:vendor/lib/libmmcamera_isp_scaler_encoder46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_scaler_video46.so:vendor/lib/libmmcamera_isp_scaler_video46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_scaler_viewfinder46.so:vendor/lib/libmmcamera_isp_scaler_viewfinder46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_sce40.so:vendor/lib/libmmcamera_isp_sce40.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_sub_module.so:vendor/lib/libmmcamera_isp_sub_module.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_isp_wb46.so:vendor/lib/libmmcamera_isp_wb46.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_m24c64s_eeprom.so:vendor/lib/libmmcamera_m24c64s_eeprom.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_pdaf.so:vendor/lib/libmmcamera_pdaf.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_pdafcamif.so:vendor/lib/libmmcamera_pdafcamif.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_ppbase_module.so:vendor/lib/libmmcamera_ppbase_module.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_sony_imx179_eeprom.so:vendor/lib/libmmcamera_sony_imx179_eeprom.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_tintless_algo.so:vendor/lib/libmmcamera_tintless_algo.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_tintless_bg_pca_algo.so:vendor/lib/libmmcamera_tintless_bg_pca_algo.so \
+	vendor/huawei/angler/proprietary/lib/libmmcamera_vpu_module.so:vendor/lib/libmmcamera_vpu_module.so \
+    vendor/huawei/angler/proprietary/lib64/libkeymaster_staging.so:vendor/libkeymaster_staging.so \
+    vendor/huawei/angler/proprietary/lib/libkeymaster_staging.so:vendor/libkeymaster_staging.so \
     vendor/huawei/angler/proprietary/lib64/libimscamera_jni.so:system/lib64/libimscamera_jni.so \
     vendor/huawei/angler/proprietary/lib64/libimsmedia_jni.so:system/lib64/libimsmedia_jni.so \
     vendor/huawei/angler/proprietary/lib64/libManufacture.so:system/lib64/libManufacture.so \
     vendor/huawei/angler/proprietary/lib64/gatekeeper.msm8994.so:vendor/lib64/hw/gatekeeper.msm8994.so \
+    vendor/huawei/angler/proprietary/lib64/gatekeeper.msm8994.so:vendor/lib64/hw/keystore.msm8994.so \
